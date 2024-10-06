f6ea17fc4335e43218439267c05435605964f35f
Emir Rıdvan Toraman <toramanemir41@gmail.com>
2024-10-06 21:36:41 +0300

    finalized

--- resources/views/back/layouts/menu.blade.php
+++ resources/views/back/layouts/menu.blade.php
@@ -202,7 +202,7 @@
                         <li class="nav-item dropdown no-arrow">
                             <a class="nav-link dropdown-toggle" href="#" id="userDropdown" role="button"
                                 data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                <span class=" text-gray-600 small">{{auth()->user()->name}}</span>  
+                                <span class="mr-2 d-none d-lg-inline text-gray-600 small">{{auth()->user()->name}}</span>  
                             <!-- Dropdown - User Information -->
                             <div class="dropdown-menu dropdown-menu-right shadow animated--grow-in"
                                 aria-labelledby="userDropdown">
